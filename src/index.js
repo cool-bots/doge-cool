12a6136e44dbda9e230f9f04bebfb8e59ff1675e
pradel <pradel.leo@gmail.com>
2018-05-11 17:42:50 +0200

    setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const { SlackBot } = require("bottender");
 const { createServer } = require("bottender/express");
+const { SlackOAuthClient } = require("messaging-api-slack");
+const BlockIo = require("block_io");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const blockIo = new BlockIo(
+  process.env.BLOCK_IO_API_KEY,
+  process.env.BLOCK_IO_SECRET_PIN,
+  process.env.BLOCK_IO_VERSION
+);
+
+const slackClient = SlackOAuthClient.connect(process.env.SLACK_TOKEN);
 
 const bot = new SlackBot({
-  accessToken: "__FILL_YOUR_TOKEN_HERE__"
+  accessToken: process.env.SLACK_TOKEN
 });
 
 bot.onEvent(async context => {});
