12594a9ba2f9c068728f3145069a2bcc9dc51b18
pradel <pradel.leo@gmail.com>
2018-05-11 17:31:35 +0200

    setup bottender

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const { SlackBot } = require('bottender');
+const { createServer } = require('bottender/express');
+
+const bot = new SlackBot({
+  accessToken: '__FILL_YOUR_TOKEN_HERE__',
+});
+
+bot.onEvent(async context => {
+});
+
+const server = createServer(bot);
+
+server.listen(3000, () => {
+  console.log('server is running on 3000 port...');
+});
