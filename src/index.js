cbc21dfe7f2248de8fe9412c2f17fa14e73896c1
pradel <pradel.leo@gmail.com>
2018-05-11 17:34:55 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-const { SlackBot } = require('bottender');
-const { createServer } = require('bottender/express');
+const { SlackBot } = require("bottender");
+const { createServer } = require("bottender/express");
 
 const bot = new SlackBot({
-  accessToken: '__FILL_YOUR_TOKEN_HERE__',
+  accessToken: "__FILL_YOUR_TOKEN_HERE__"
 });
 
-bot.onEvent(async context => {
-});
+bot.onEvent(async context => {});
 
 const server = createServer(bot);
 
 server.listen(3000, () => {
-  console.log('server is running on 3000 port...');
+  console.log("server is running on 3000 port...");
 });
