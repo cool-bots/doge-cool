706cee6f7d700535a9f731f08f374b74e0db3d63
a-w-e-s-o-m-e <46979049+a-w-e-s-o-m-e@users.noreply.github.com>
2019-01-25 17:14:19 +0100

    Added sending TX for rain. OMG THIS CODE IS SO UGLY!

--- src/blockchain/rain.js
+++ src/blockchain/rain.js
@@ -6,7 +6,7 @@ exports.rain = async (context, block_io, slackClient) => {
   const userId = context.session.user.id;
   const channelId = context.session.channel.id;
   let members = await slackClient.getAllConversationMembers(channelId);
-  let [, amount] = context.event.text.split(" ");
+  let [, , amount] = context.event.text.split(" ");
   amount = Number(amount);
 
   // Remove the current user and the bot so it does not get a shower
@@ -18,9 +18,20 @@ exports.rain = async (context, block_io, slackClient) => {
     await context.sendText(`Much sad! Not enough doge :( `);
   } else if (amount === minCoins || amount < members.length * 2) {
     const member = utils.getRandomArrayElements(members, 1);
-    // TODO real send
-    await context.sendText(
-      `${utils.generateCongrats()} <@${member}> you just received ${amount} doge. ${utils.generateWow()}`
+    block_io.withdraw_from_labels(
+      {
+        from_labels: context.session.user.id,
+        to_label: member,
+        amount: amount,
+        pin: process.env.BLOCK_IO_SECRET_PIN
+      },
+      async (error, data) => {
+        console.log(data);
+        if (error) return console.log("Error occured:", error.message);
+        await context.sendText(
+          `${utils.generateCongrats()} <@${member}> you just received ${amount} doge. ${utils.generateWow()}`
+        );
+      }
     );
   } else {
     console.log(",", members.length);
@@ -32,10 +43,22 @@ exports.rain = async (context, block_io, slackClient) => {
         Math.round(members.length / 2)
       );
     }
-    members.forEach(member => {
-      // TODO send transaction
-      console.log(member, amount / members.length);
-    });
+    console.log(members, amount / members.length);
+    console.log(
+      `Sending to ${members.join()} , ${amount / members.length} doge each!`
+    );
+
+    block_io.withdraw_from_labels(
+      {
+        from_labels: context.session.user.id,
+        to_labels: members.join(), //member,
+        amounts: new Array(members.length).fill(amount / members.length).join(), //amount / members.length,
+        pin: process.env.BLOCK_IO_SECRET_PIN
+      },
+      async (error, data) => {
+        if (error) return console.log("Error occured:", error.message);
+      }
+    );
     await context.sendText(
       `${utils.generateCongrats()} ${members.map(
         member => `<@${member}>`
