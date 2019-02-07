4bdc849cc72f15446952281423d2952a78b5673c
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2019-02-07 19:41:36 +0100

    fix typo

--- src/blockchain/help.js
+++ src/blockchain/help.js
@@ -1,5 +1,5 @@
 exports.help = async context => {
   await context.sendText(
-    `Much useful. Very help. https://github.com/cool-bots/doge-cool}`
+    `Much useful. Very help. https://github.com/cool-bots/doge-cool`
   );
 };
