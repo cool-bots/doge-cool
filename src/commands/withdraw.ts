44be8a92ee121e21b659bddda78dba57034fa420
iourisorokine <isorokine@gmail.com>
2020-06-18 18:47:11 +0200

    fix withdrawal

--- src/commands/withdraw.ts
+++ src/commands/withdraw.ts
@@ -2,8 +2,6 @@ import * as utils from '../lib/utils';
 import { getAddressByLabel } from '../integrations/blockIo';
 import { Context } from '../types/bottender';
 import help from './help';
-import coinstring from 'coinstring';
-import { promisify } from 'util';
 
 export const withdraw = async (context: Context, blockIo: any) => {
   const fromLabel = context.session.user.id;
@@ -11,26 +9,28 @@ export const withdraw = async (context: Context, blockIo: any) => {
   const availableBalance = Number(fromAddress.data.available_balance);
 
   let [, , toAddress, amount] = context.event.text.split(' ');
-  if (!toAddress || !amount || isNaN(Number(amount))) {
+  if (!toAddress || !amount || isNaN(+amount)) {
     help(context);
     return;
   }
 
-  if (availableBalance < Number(amount)) {
+  if (availableBalance < +amount) {
     await context.sendText(
       `Looks like this amount is above your balance - you only have ${availableBalance} doge!`
     );
     return;
   }
 
-  if (availableBalance < 2) {
+  if (+amount < 2) {
     await context.sendText(
       `Such low amount! Much too low to cover fees! ${utils.generateWow()}`
     );
     return;
   }
 
-  var isValid = coinstring.validate(0x1e, toAddress);
+  const isValid = /[AD9]{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}/.test(
+    toAddress
+  );
   if (!isValid) {
     await context.sendText(
       `Oh no, much typo in your address. It can't be ${toAddress} ?!`
@@ -39,11 +39,20 @@ export const withdraw = async (context: Context, blockIo: any) => {
   }
 
   try {
-    await promisify(blockIo.withdraw_from_labels)({
-      from_labels: fromLabel,
-      to_addresses: toAddress,
-      amounts: amount,
-      pin: process.env.BLOCK_IO_SECRET_PIN,
+    await new Promise((resolve, reject) => {
+      blockIo.withdraw_from_labels({
+        from_labels: fromLabel,
+        to_addresses: toAddress,
+        amounts: amount,
+        pin: process.env.BLOCK_IO_SECRET_PIN,
+      });
+      (error: any, res: any) => {
+        if (error) {
+          return reject(error);
+        }
+        context.sendText('Successful withdrawal');
+        resolve();
+      };
     });
     await context.sendText(
       `${utils.generateCongrats()} Such rich! Much withdrawal!`
@@ -51,7 +60,6 @@ export const withdraw = async (context: Context, blockIo: any) => {
     return;
   } catch (error) {
     await context.sendText(error.message);
-    return;
   }
 };
 
